8c1c7e132b3a5f42b33ab99c53cbe2b0dba64597
Matthew Jeshua Martin <matthew-martin@programmer.net>
2023-12-13 11:25:02 -0500

    Created the UserService Interface

--- src/main/java/user/UserService.java
+++ src/main/java/user/UserService.java
@@ -1,2 +1,8 @@
-package user;public interface UserService {
+package user;
+
+public interface UserService {
+    public void saveUser(User user);
+    public void updateUser(int userId, User user);
+    public User getUserById(int userId);
+    public void deleteUserById(int userId);
 }
