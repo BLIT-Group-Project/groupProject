60f928c6651bd37b7a8fe5d231b5a5756c176505
Matthew Martin <matthew-martin@programmer.net>
2023-12-17 18:43:48 -0500

    Added comments to help with some of the bugs

--- src/main/java/transaction/TransactionServiceImpl.java
+++ src/main/java/transaction/TransactionServiceImpl.java
@@ -15,17 +15,15 @@ public class TransactionServiceImpl implements TransactionServices {
     @Override
     public void createTransaction(Transaction transaction) {
         transactionDb.add(transaction);
-
-
     }
 
     @Override
     public void updateTransaction(Transaction transaction) {
+        // If updating the transaction need to find the transaction by transactionId before you can make changes to the transaction.
+        // Will want to know more about your idea on the updateTransaction.
+        
         transaction.setAmount(transaction.getAmount());
         transaction.setTimeStamp(transaction.getTimeStamp());
-
-
-
     }
 //        transaction.setTransactionId(transaction.getFromAccountId());
 //        transaction.setTimeStamp(transaction.getTimeStamp());
@@ -38,6 +36,7 @@ public class TransactionServiceImpl implements TransactionServices {
         for (Transaction transaction : transactionDb) {
             if (transaction.getTransactionId() == transactionId) {
                 transactionToRemove = transaction;
+                // I would remove this as it will break out of the For loop
                 break;
             }
             if (transactionToRemove != null) {
@@ -49,6 +48,9 @@ public class TransactionServiceImpl implements TransactionServices {
 
     @Override
     public void displayTransaction() {
+        // This one we are going want to makesure to limit who sees what transactions by checking if their accountId is in the FromId or ToId
+        // Just like a bank we would not want to have every customer see every transaction that goes through the bank itself just information
+        // Important to them.
         if(transactionDb.isEmpty()){
             System.out.println("No transaction to display ");
         }else
