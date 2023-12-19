8a2be0af0e9f33e7dfb80d951f01e584a3defa32
Matthew Jeshua Martin <matthew-martin@programmer.net>
2023-12-19 11:56:30 -0500

    SQL file to create database

---
+++ src/main/resources/sql/scratch.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE binary_bank_system;
+
+USE binary_bank_system;
+
+CREATE TABLE users (
+    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(300) NOT NULL,
+    salt VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE accounts (
+     account_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     user_id INT NOT NULL,
+     balance DECIMAL(7,2) NOT NULL DEFAULT 0.00,
+     account_type VARCHAR(20) NOT NULL DEFAULT '',
+     interest_rate DECIMAL(4,2) NOT NULL DEFAULT 0.00,
+     minimum_payment DECIMAL(7,2) DEFAULT 0.00,
+     credit_limit DECIMAL(7,2) DEFAULT 0.00,
+     FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE transactions (
+    transaction_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    from_id INT  NOT NULL,
+    to_id INT  NOT NULL,
+    amount DECIMAL(7,2) NOT NULL DEFAULT 0.0,
+    _timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    transaction_status VARCHAR(50) NOT NULL DEFAULT 'PENDING',
+    transaction_type VARCHAR(50) NOT NULL DEFAULT '',
+    FOREIGN KEY(from_id) REFERENCES accounts(account_id),
+    FOREIGN KEY(to_id) REFERENCES accounts(account_id)
+);
